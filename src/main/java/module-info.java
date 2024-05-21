e9ec0cc10acba1b967c18c976793355d45ac0531
Dal S Garbuja Pun <dsgarbujapun@gmail.com>
2024-05-21 11:05:56 +1000

    create project structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,16 @@
 module com.example.merobank {
     requires javafx.controls;
     requires javafx.fxml;
+    requires de.jensd.fx.glyphs.fontawesome;
+    requires java.sql;
+    requires org.xerial.sqlitejdbc;
 
 
     opens com.example.merobank to javafx.fxml;
     exports com.example.merobank;
+    exports com.example.merobank.controller;
+    exports com.example.merobank.controller.client;
+    exports com.example.merobank.controller.admin;
+    exports com.example.merobank.model;
+    exports com.example.merobank.view;
 }
